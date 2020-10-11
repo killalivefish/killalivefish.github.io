4586fd3795168536e84892331c39e6183369188d
killalivefish <969559629@qq.com>
2020-10-10 23:55:35 -0500

    change process

--- js/main.js
+++ js/main.js
@@ -4,19 +4,15 @@ console.log("hello");
 
 function process(){
     var page = document.getElementById('page');
-    // console.log(page.offsetHeight);
-    // console.log(page.scrollTop);
-    var pageH = page.offsetHeight;
-
-    var scrollerH = document.body.scrollTop;
-    // console.log("scrollTop:"+document.body.scrollTop);
-    // console.log("scrollDowm:"+document.body.scrollHeight);
-    
-    var h = 100*(scrollerH/pageH);
-
-    var apple = document.getElementById('process');
-    apple.style.width= h+'%';
-    apple.style.background='red';
+    if(page){
+        var pageH = document.body.scrollHeight-document.body.clientHeight;
+        var scrollerH = document.body.scrollTop;
+        var h = 100*(scrollerH/pageH);
+        
+        var apple = document.getElementById('process');
+        apple.style.width= h+'%';
+        apple.style.background='green';
+    }
 }
 
 window.onscroll = process;
