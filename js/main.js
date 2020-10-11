9d7f917e0734e45cbb133fe673c86d8cde2405fb
killalivefish <969559629@qq.com>
2020-10-11 01:15:57 -0500

    change something

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-console.log("hello");
-
-
+console.log("-----寻找之人必有所获-------");
+console.log("--联系方式969559629@qq.com--");
 
 function process(){
     var page = document.getElementById('page');
@@ -16,3 +15,25 @@ function process(){
 }
 
 window.onscroll = process;
+
+function showAsideBar(){
+    let asideBar = document.getElementById('aside');
+    if(asideBar){
+        var asideDis = asideBar.style.display;
+        if(asideDis=='none'){
+            asideBar.style.display='block';
+        }else{
+            asideBar.style.display='none';
+        }
+    }
+}
+
+function showInnerItem(item){
+    var innerUl = document.getElementById(item);
+    var innerDis = innerUl.style.display;
+    if(innerDis=='block'){
+        innerUl.style.display='none';
+    }else{
+        innerUl.style.display='block';
+    }
+}
